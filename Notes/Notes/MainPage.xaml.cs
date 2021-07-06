5a6e2c96b0dc72440044b54abffc44b2db94d797
Tisha1230 <tishashrestha101@gmail.com>
2021-07-06 01:09:14 -0700

    Completed Notes Mobile App

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+
+        protected override void OnAppearing()
+        {
+            var notes = new List<Note>();
+            var files=  Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.notes.txt");
+            
+            foreach (var filename in files)
+            {
+                var note = new Note
+                {
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename),
+                    FileName = filename
+                };
+                notes.Add(note);
+            }
+            NotesListView.ItemsSource = notes.OrderBy(n => n.Date).ToList(); //Binding with UI using lambda expression
+        }
+
+        private async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new AddNotePage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        private async void OnNoteItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            await Navigation.PushModalAsync(new AddNotePage
+            {
+                BindingContext = (Note)e.SelectedItem
+            });
+        }
+    }
+}
